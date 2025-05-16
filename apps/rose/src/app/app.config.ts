3bee7606f1b3837e21766daa762b6dba8f35fdb0
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-05-16 16:46:31 +0300

    create project install packages

---
+++ apps/rose/src/app/app.config.ts
@@ -0,0 +1,38 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideClientHydration(withEventReplay()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    MessageService,
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'light-mode',
+          cssLayer: false,
+        },
+      },
+    }),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
+};
