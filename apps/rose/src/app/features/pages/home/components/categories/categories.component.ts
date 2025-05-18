db7d90717f2d9823980ae7358fabf5e4adc9e2a9
karim Ashraf <79676051+karimprime@users.noreply.github.com>
2025-05-18 23:13:52 +0300

    Update categories.component.ts

--- apps/rose/src/app/features/pages/home/components/categories/categories.component.ts
+++ apps/rose/src/app/features/pages/home/components/categories/categories.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-categories',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
 })
