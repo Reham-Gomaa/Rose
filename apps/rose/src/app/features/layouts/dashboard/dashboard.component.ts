9994b0c57f2ab23e9d2c14ba8c12c8fd0ee91b4c
karim Ashraf <79676051+karimprime@users.noreply.github.com>
2025-05-18 23:09:39 +0300

    Update dashboard.component.ts

--- apps/rose/src/app/features/layouts/dashboard/dashboard.component.ts
+++ apps/rose/src/app/features/layouts/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../../pages/navbar/navbar.component";
+import { FooterComponent } from "./features/pages/footer/footer.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet ,CommonModule, NavbarComponent],
+  imports: [RouterOutlet , NavbarComponent , FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
