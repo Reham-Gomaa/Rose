41b8b12ae99b52aac063664057b9db31348e4800
karim Ashraf <79676051+karimprime@users.noreply.github.com>
2025-05-18 23:14:32 +0300

    Update gifts.component.ts

--- apps/rose/src/app/features/pages/home/components/gifts/gifts.component.ts
+++ apps/rose/src/app/features/pages/home/components/gifts/gifts.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gifts',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './gifts.component.html',
   styleUrl: './gifts.component.scss',
 })
