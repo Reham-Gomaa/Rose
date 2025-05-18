d34715c17ab3143cd07a988b493f3b1498ef676c
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-05-18 22:19:06 +0300

    router outlet

--- apps/rose/src/app/app.component.ts
+++ apps/rose/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { FooterComponent } from "./features/pages/footer/footer.component";
 
 @Component({
-  imports: [RouterModule, ButtonModule],
+  imports: [RouterModule, ButtonModule, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/rose/src/app/features/layouts/dashboard/dashboard.component.ts
+++ apps/rose/src/app/features/layouts/dashboard/dashboard.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from "../../pages/navbar/navbar.component";
-import { FooterComponent } from "../../pages/footer/footer.component";
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "../../pages/navbar/navbar.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet ,CommonModule, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet ,CommonModule, NavbarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
