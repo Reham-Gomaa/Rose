b09809f8c8f5a9e7d435caa587cb74d7b7d97fc6
karim Ashraf <79676051+karimprime@users.noreply.github.com>
2025-05-18 23:14:13 +0300

    Update gallery.component.ts

--- apps/rose/src/app/features/pages/home/components/gallery/gallery.component.ts
+++ apps/rose/src/app/features/pages/home/components/gallery/gallery.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss',
 })
