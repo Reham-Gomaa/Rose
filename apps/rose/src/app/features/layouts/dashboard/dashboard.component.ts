d9ebdbc4a17c2e805ac41502953ca7031b99047f
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-05-19 13:15:09 +0300

    resolve conflict

--- apps/rose/src/app/features/layouts/dashboard/dashboard.component.ts
+++ apps/rose/src/app/features/layouts/dashboard/dashboard.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-import { NavbarComponent } from "../navbar/navbar.component";
-import { FooterComponent } from "../footer/footer.component";
-=======
 import { FooterComponent } from "../footer/footer.component";
 import { NavbarComponent } from '../navbar/navbar.component';
->>>>>>> 92e3719f12e1287273f0b6cc1eba86d1cbe980c5
 
 @Component({
   selector: 'app-dashboard',
