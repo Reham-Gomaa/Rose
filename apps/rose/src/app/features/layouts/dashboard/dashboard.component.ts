32cb430050702bc23accef7e7577ffd88ecd40ac
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-05-19 12:04:07 +0300

    modify folder structure

--- apps/rose/src/app/features/layouts/dashboard/dashboard.component.ts
+++ apps/rose/src/app/features/layouts/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "../../pages/navbar/navbar.component";
-import { FooterComponent } from "./features/pages/footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet , NavbarComponent , FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
