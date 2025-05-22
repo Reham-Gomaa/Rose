5ac06c61dbf7aeb562df9b2d78576d4e76e057eb
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-05-22 11:14:12 +0300

    modify title value to be descriptive

--- apps/rose/src/app/features/pages/home/components/gifts/gifts.component.ts
+++ apps/rose/src/app/features/pages/home/components/gifts/gifts.component.ts
@@ -12,15 +12,15 @@ import { TagModule } from 'primeng/tag';
 export class GiftsComponent implements OnInit {
 
   carouselList: any = [
-    { id: 1, source: "./images/1.jpeg", title: "gifts" },
-    { id: 2, source: "./images/2.jpeg", title: "gifts" },
-    { id: 3, source: "./images/3.jpeg", title: "gifts" },
-    { id: 4, source: "./images/4.jpeg", title: "gifts" },
+    { id: 1, source: "./images/1.jpeg", title: "pink roses in white vase with white and pink balloons" },
+    { id: 2, source: "./images/2.jpeg", title: "christmas gift with gray ribbon and orange roses" },
+    { id: 3, source: "./images/3.jpeg", title: "pink christmas gifts with pink ribbons" },
+    { id: 4, source: "./images/4.jpeg", title: "christmas gift with red ribbon" },
   ]
   pics: any = [
-    { id: 1, source: "./images/Confetti lying near present.png", title: "red christmas gifts" },
-    { id: 2, source: "./images/Top view hand holding gift box on work space.png", title: "red christmas gifts" },
-    { id: 3, source: "./images/Christmas shopping composition with presents and cart.png", title: "red christmas gifts" },
+    { id: 1, source: "./images/Confetti lying near present.png", title: "christmas gift with red ribbon" },
+    { id: 2, source: "./images/Top view hand holding gift box on work space.png", title: "christmas gift with golden ribbon" },
+    { id: 3, source: "./images/Christmas shopping composition with presents and cart.png", title: "colorful christmas ribbons with shopping cart" },
   ]
 
   responsiveOptions: any[] | undefined;
