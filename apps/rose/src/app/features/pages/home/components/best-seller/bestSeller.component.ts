6bb90ded7ee26b8312a12cc4653ac81da300e52b
karim Ashraf <karimashraf000@gmail.com>
2025-05-28 20:05:54 +0300

    Finish Section Gallery

--- apps/rose/src/app/features/pages/home/components/best-seller/bestSeller.component.ts
+++ apps/rose/src/app/features/pages/home/components/best-seller/bestSeller.component.ts
@@ -1,9 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { CardItemComponent } from '../../../../../shared/components/ui/card-item/card-item.component';
+import { ButtonComponent } from '../../../../../shared/components/ui/button/button.component';
+
+//PrimeNg
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { BestSellerService } from '../../../../../shared/services/best-seller/best-seller.service';
+import { BestSeller, BestSellerRes } from '../../../../../core/interfaces/best-seller';
 
 @Component({
   selector: 'app-best-seller',
-  imports: [],
+  standalone: true,
+  imports: [CarouselModule, ButtonModule, CardItemComponent, ButtonComponent],
   templateUrl: './bestSeller.component.html',
-  styleUrl: './bestSeller.component.scss',
+  styleUrls: ['./bestSeller.component.scss']
 })
-export class BestSellerComponent {}
+export class BestSellerComponent implements OnInit, OnDestroy {
+
+  private readonly bestsellerService = inject(BestSellerService);
+  bestSellers: BestSeller[] = [];
+
+  private subscription: Subscription | undefined;
+
+  responsiveOptions = [
+    {
+      breakpoint: '1199px',
+      numVisible: 2,
+      numScroll: 1
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ];
+
+  ngOnInit(): void {
+    this.getBestSellersData();
+  }
+
+
+  getBestSellersData(): void {
+    this.subscription = this.bestsellerService.getBestSellers().subscribe({
+      next: (res: BestSellerRes) => {
+        this.bestSellers = res.bestSeller;
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+}
---
+++ apps/rose/src/app/shared/components/ui/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+
+  @Input() text!: string;
+  @Input() isIcon!: boolean;
+  @Input() borderRadius!: string;
+  @Input() padding!: string;
+  @Input() margin!: string;
+}
