be0831021ca978f58fca53d2162c4a58ef958cc9
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-05-29 12:23:41 +0300

    build testimonials skeleton

--- apps/rose/src/app/features/pages/home/components/testimonials/testimonials.component.ts
+++ apps/rose/src/app/features/pages/home/components/testimonials/testimonials.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-testimonials',
-  imports: [],
+  imports: [ CarouselModule, ButtonModule ],
   templateUrl: './testimonials.component.html',
   styleUrl: './testimonials.component.scss',
 })
-export class TestimonialsComponent {}
+export class TestimonialsComponent {
+    responsiveOptions: any[] | undefined = [
+    // {
+    //   breakpoint: '1199px',
+    //   numVisible: 2,
+    //   numScroll: 1
+    // },
+    {
+      breakpoint: '768px',
+      numVisible: 4,
+      numScroll: 1
+    }
+  ];
+}
