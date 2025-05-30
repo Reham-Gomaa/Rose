418275e24965c6f3427fea94f76d2d582a42cb54
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-05-30 19:55:05 +0300

    improving seo and accessibility, modify carousel customization

--- apps/rose/src/app/features/pages/home/components/gifts/gifts.component.ts
+++ apps/rose/src/app/features/pages/home/components/gifts/gifts.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { CarouselModule } from 'primeng/carousel';
-import { TagModule } from 'primeng/tag';
 import { carouselListInterface, picsInterface, responsiveOptionsInterface } from '../../../../../core/interfaces/special-gifts.interface';
 import { ButtonComponent } from "../../../../../shared/components/ui/button/button.component";
 
+// primeNg ...
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
 @Component({
   selector: 'app-gifts',
   imports: [CarouselModule, ButtonModule, TagModule, ButtonComponent],
@@ -19,18 +20,18 @@ export class GiftsComponent {
     { id: 3, source: "./images/gifts/3.jpeg", title: "pink christmas gifts with pink ribbons" },
     { id: 4, source: "./images/gifts/4.jpeg", title: "christmas gift with red ribbon" },
   ]
+
+  responsiveOptions: responsiveOptionsInterface[] | undefined = [
+    {
+      breakpoint: '575px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ]
+
   pics: picsInterface[] = [
-    { id: 1, source: "./images/gifts/Confetti lying near present.png", title: "christmas gift with red ribbon" , heading2:"Gifts Box" , heading1:"Awesome Gifts Box Collections", btnText:'Shop Now' },
-    { id: 2, source: "./images/gifts/Top view hand holding gift box on work space.png", title: "christmas gift with golden ribbon", heading2:"Occasion Gifts" , heading1:"Best Occasion Gifts collections", btnText:'Discover Now' },
-    { id: 3, source: "./images/gifts/Christmas shopping composition with presents and cart.png", title: "colorful christmas ribbons with shopping cart", heading2:"Occasion Gifts" , heading1:"Combo Sets Gift Box Up To 50% Off", btnText:'Discover Now' },
+    { id: 1, source: "./images/gifts/Confetti lying near present.png", title: "christmas gift with red ribbon", heading2: "Gifts Box", heading1: "Awesome Gifts Box Collections", btnText: 'Shop Now' },
+    { id: 2, source: "./images/gifts/Top view hand holding gift box on work space.png", title: "christmas gift with golden ribbon", heading2: "Occasion Gifts", heading1: "Best Occasion Gifts collections", btnText: 'Discover Now' },
+    { id: 3, source: "./images/gifts/Christmas shopping composition with presents and cart.png", title: "colorful christmas ribbons with shopping cart", heading2: "Occasion Gifts", heading1: "Combo Sets Gift Box Up To 50% Off", btnText: 'Discover Now' },
   ];
-
-  responsiveOptions: responsiveOptionsInterface[] | undefined  = [
-      {
-        breakpoint: '575px',
-        numVisible: 1,
-        numScroll: 1
-      }
-    ]
-
 }
