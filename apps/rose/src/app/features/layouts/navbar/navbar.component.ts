4323cd975a4f2340fff144fbda2a425edd2983b9
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-06-08 16:50:18 +0300

    improve gifts tamplate code, modify language selector design

--- apps/rose/src/app/features/layouts/navbar/navbar.component.ts
+++ apps/rose/src/app/features/layouts/navbar/navbar.component.ts
@@ -1,17 +1,35 @@
-import { Component, inject } from '@angular/core';
-import { TranslationService } from '../../../core/services/translation/translation.service';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { TranslationService } from '../../../core/services/translation/translation.service';
+import { isPlatformBrowser } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [ TranslatePipe ],
+  imports: [TranslatePipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   private readonly translationService = inject(TranslationService);
+  private readonly platformId = inject(PLATFORM_ID);
+  currentLang !:string;
 
-  changeLang(lang: string) {
+  changeLang(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    const lang = selectElement.value;
     this.translationService.changeLang(lang);
   }
+
+  ngOnInit(): void {
+    if(isPlatformBrowser(this.platformId)){
+      if(localStorage.getItem('lng')){
+          this.currentLang = localStorage.getItem('lng')!;
+      }else{
+        this.currentLang = this.translationService.defaultLang;
+      }
+    }
+  }
+
 }
