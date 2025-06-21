e2773870d3f01255d837482e96e783464831a485
karim Ashraf <karimashraf000@gmail.com>
2025-06-21 17:09:24 +0300

    fix: 🐛 Fix Sorting and handle it

--- apps/rose/src/app/features/pages/all-categories/components/filter-categories/components/sorting/sorting.component.ts
+++ apps/rose/src/app/features/pages/all-categories/components/filter-categories/components/sorting/sorting.component.ts
@@ -8,5 +8,31 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './sorting.component.scss'
 })
 export class SortingComponent {
+  sortOptions = [
+    {
+      id: 'sort-low-high',
+      value: 'price-asc',
+      label: 'all-categories.cardSort.Price: Low to High',
+      checked: false
+    },
+    {
+      id: 'sort-high-low',
+      value: 'price-desc',
+      label: 'all-categories.cardSort.Price: High to Low',
+      checked: false
+    },
+    {
+      id: 'sort-a-z',
+      value: 'alpha-asc',
+      label: 'all-categories.cardSort.Alphabetical: A-Z',
+      checked: false
+    },
+    {
+      id: 'sort-z-a',
+      value: 'alpha-desc',
+      label: 'all-categories.cardSort.Alphabetical: Z-A',
+      checked: false
+    }
+  ];
 
 }
