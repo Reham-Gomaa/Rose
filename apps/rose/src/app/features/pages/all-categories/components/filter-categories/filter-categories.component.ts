ae094f685fd2f3a1c6878494d0a2df43919460c1
karim Ashraf <karimashraf000@gmail.com>
2025-06-22 00:52:29 +0300

    fix: 🐛 edit pass

--- apps/rose/src/app/features/pages/all-categories/components/filter-categories/filter-categories.component.ts
+++ apps/rose/src/app/features/pages/all-categories/components/filter-categories/filter-categories.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { SortingComponent } from "./components/sorting/sorting.component";
 import { InputBtnComponent } from "../../../../../shared/components/ui/input-btn/input-btn.component";
 import { TranslatePipe } from '@ngx-translate/core';
-import { FilterBrandsComponent } from "./components/filter-brands/filter-occasions.component";
+import { FilterBrandsComponent } from "./components/filter-occasions/filter-occasions.component";
 import { FilterPriceComponent } from "./components/filter-price/filter-price.component";
 import { FilterSalesComponent } from "./components/filter-sales/filter-sales.component";
 import { FilterRatingComponent } from "./components/filter-rating/filter-rating.component";
