064fb8f404ecaadc590e89426de37c936ce51279
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-06-22 22:36:40 +0300

    handle checked card

--- apps/rose/src/app/features/pages/all-categories/components/filter-categories/components/filter-occasions/filter-occasions.component.ts
+++ apps/rose/src/app/features/pages/all-categories/components/filter-categories/components/filter-occasions/filter-occasions.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CheckedCardComponent } from "../../../../../../../shared/components/business/checkbox/checked-card.component";
 import { FilterCardComponent } from "../../../../../../../shared/components/ui/filter-card/filter-card.component";
-import { staticFilterItem } from '../../../../../../../core/interfaces/static-filter-item.interface';
+import { staticFilterItem } from './../../../../../../../core/interfaces/static-filter-item.interface';
 
 @Component({
   selector: 'app-filter-brands',
@@ -11,11 +11,11 @@ import { staticFilterItem } from '../../../../../../../core/interfaces/static-fi
 })
 export class FilterBrandsComponent {
 
-  brands: staticFilterItem[] = [
-    { _id: '1-brand', category: 'Wedding' },
-    { _id: '2-brand', category: 'Apology' },
-    { _id: '3-brand', category: 'Graduation' },
-    { _id: '4-brand', category: 'Anniversary' }
+  occasions: staticFilterItem[] = [
+    { _id: '1-brand', category: 'Wedding'},
+    { _id: '2-brand', category: 'Apology'},
+    { _id: '3-brand', category: 'Graduation'},
+    { _id: '4-brand', category: 'Anniversary'}
   ];
 
   selectedItems: string[] = [];
