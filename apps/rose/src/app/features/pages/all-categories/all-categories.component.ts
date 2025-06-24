d6ae2efef749908955b05a74d693a0c72cc3c7e5
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-06-24 18:02:45 +0300

    in this commit i loaded products inside next

--- apps/rose/src/app/features/pages/all-categories/all-categories.component.ts
+++ apps/rose/src/app/features/pages/all-categories/all-categories.component.ts
@@ -29,8 +29,7 @@ export class AllCategoriesComponent implements OnInit, OnDestroy {
 
   filterDrawerVisible = false;
 
-  private readonly _productsService = inject(ProductsService);
-  private readonly _store = inject(Store);
+
 
   products = signal<Product[]>([]);
   loading = signal(true);
@@ -38,14 +37,19 @@ export class AllCategoriesComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.loadProducts();
-    this._store.dispatch(sortActions.loadProducts({
-      products:this.products()
-    }))
+
     this._store.select(sortSelectors.sortedProducts).subscribe({
       next: (products) =>{
         this.products.set(products)
+        console.log(this.products());
       }
     })
+  }
+
+  addProductsToStore(){
+    this._store.dispatch(sortActions.loadProducts({
+      products:this.products()
+    }))
   }
 
   private loadProducts() {
@@ -54,6 +58,8 @@ export class AllCategoriesComponent implements OnInit, OnDestroy {
       next: (res) => {
         this.products.set(res.products || []);
         this.loading.set(false);
+        this.loadProducts()
+
       },
       error: () => {
         this.loading.set(false);
