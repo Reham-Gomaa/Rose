c8d0543c5b0a1d303a4baf7b50483c1a6a5b1575
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-06-26 00:19:54 +0300

    implement slider and translation

--- apps/rose/src/app/features/pages/all-categories/components/filter-categories/components/filter-price/filter-price.component.ts
+++ apps/rose/src/app/features/pages/all-categories/components/filter-categories/components/filter-price/filter-price.component.ts
@@ -1,9 +1,15 @@
 import { Component } from "@angular/core";
+import { FormsModule } from '@angular/forms';
+import { Slider } from 'primeng/slider';
+import { FilterCardComponent } from "../../../../../../../shared/components/ui/filter-card/filter-card.component";
+import { TranslatePipe } from "@ngx-translate/core";
 
 @Component({
   selector: "app-filter-price",
-  imports: [],
+  imports: [FilterCardComponent, TranslatePipe, FormsModule, Slider],
   templateUrl: "./filter-price.component.html",
   styleUrl: "./filter-price.component.scss",
 })
-export class FilterPriceComponent {}
+export class FilterPriceComponent {
+  rangeValues: number[] = [20, 80];
+}
--- apps/rose/src/app/shared/components/ui/filter-card/filter-card.component.ts
+++ apps/rose/src/app/shared/components/ui/filter-card/filter-card.component.ts
@@ -1,12 +1,14 @@
 import { booleanAttribute, Component, Input } from "@angular/core";
 
+
 @Component({
   selector: "app-filter-card",
-  imports: [],
+  imports: [ ],
   templateUrl: "./filter-card.component.html",
   styleUrl: "./filter-card.component.scss",
 })
 export class FilterCardComponent {
   @Input({ transform: booleanAttribute }) underlineTitle = true;
   @Input() title!: string;
+
 }
