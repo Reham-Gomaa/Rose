f8940b4a62b3a2acdbdf5f511031302ffadc1baf
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-06-25 21:28:30 +0300

    filter-rating component

--- apps/rose/src/app/features/pages/all-categories/components/filter-categories/components/filter-price/filter-price.component.ts
+++ apps/rose/src/app/features/pages/all-categories/components/filter-categories/components/filter-price/filter-price.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FilterCardComponent } from "../../../../../../../shared/components/ui/filter-card/filter-card.component";
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-filter-price',
-  imports: [],
+  imports: [FilterCardComponent, TranslatePipe],
   templateUrl: './filter-price.component.html',
   styleUrl: './filter-price.component.scss'
 })
