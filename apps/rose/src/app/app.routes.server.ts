7cece4e8a27c0ed46996c3e572a65db33cd65d3d
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-07-16 23:39:02 +0300

    without id

--- apps/rose/src/app/app.routes.server.ts
+++ apps/rose/src/app/app.routes.server.ts
@@ -10,7 +10,7 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server,
   },
   {
-    path: "dashboard/details/:id",
+    path: "dashboard/details",
     renderMode: RenderMode.Client,
   },
   {
