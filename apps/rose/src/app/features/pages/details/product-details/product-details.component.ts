0c9811bfababd43c9b70fa1ebd9322b25ea515bb
Habiba <Habiba.em7@gmail.com>
2025-07-17 20:29:07 +0300

    feat: 🎸 product reviews logic

--- apps/rose/src/app/features/pages/details/product-details/product-details.component.ts
+++ apps/rose/src/app/features/pages/details/product-details/product-details.component.ts
@@ -1,16 +1,35 @@
-import { Component, input, InputSignal } from "@angular/core";
-import { CommonModule,NgOptimizedImage } from "@angular/common";
-import { Product } from "../../../../core/interfaces/carditem.interface";
+import { Component, InputSignal, input, signal, effect } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { DialogModule } from 'primeng/dialog';
+import { Product } from '../../../../core/interfaces/carditem.interface';
 
 @Component({
-  selector: "app-product-details",
-  imports: [CommonModule,NgOptimizedImage],
-  templateUrl: "./product-details.component.html",
-  styleUrl: "./product-details.component.scss",
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage, DialogModule],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.scss'
 })
 export class ProductDetailsComponent {
+  productDetails = input.required<Product>();
+  currentImage = signal<string>('');
+  showModal = signal<boolean>(false);
 
-  productDetails:InputSignal<Product> = input.required<Product>();
+  constructor() {
+    // Watch for input changes
+    effect(() => {
+      if (this.productDetails()) {
+        this.currentImage.set(this.productDetails().imgCover);
+      }
+    });
+  }
 
+  onThumbnailClick(image: string): void {
+    this.currentImage.set(image);
+  }
 
+  openImageModal(): void {
+    // this.currentImage.set(image);
+    this.showModal.set(true);
+  }
 }
--- apps/rose/src/app/shared/components/ui/card-item/card-item.component.ts
+++ apps/rose/src/app/shared/components/ui/card-item/card-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from "@angular/core";
 import { Router } from "@angular/router";
 // Images
-import { NgOptimizedImage } from "@angular/common";
+import { CommonModule, NgOptimizedImage } from "@angular/common";
 // Translation
 import { TranslatePipe } from "@ngx-translate/core";
 // Interfaces
