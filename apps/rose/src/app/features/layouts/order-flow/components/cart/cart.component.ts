2437417429fc9043552954a4e1f62a42c1d848ac
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-08-03 22:55:54 +0300

    feat: 🎸 translate and animation
    add translation and animation to empty cart page

--- apps/rose/src/app/features/layouts/order-flow/components/cart/cart.component.ts
+++ apps/rose/src/app/features/layouts/order-flow/components/cart/cart.component.ts
@@ -1,11 +1,20 @@
-import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
+// @angular
+import { Component, inject } from "@angular/core";
+// @ngx
+import { TranslatePipe } from "@ngx-translate/core";
+// shared-comp
 import { ButtonComponent } from "@rose/shared_Components_ui/button/button.component";
+// Animation
+import { fadeTransition } from "@rose/core_services/translation/fade.animation";
+import { TranslationService } from "@rose/core_services/translation/translation.service";
 
 @Component({
   selector: "app-cart",
-  imports: [CommonModule, ButtonComponent],
+  imports: [ButtonComponent, TranslatePipe],
   templateUrl: "./cart.component.html",
   styleUrl: "./cart.component.scss",
+  animations: [fadeTransition],
 })
-export class CartComponent {}
+export class CartComponent {
+  readonly translationService = inject(TranslationService);
+}
