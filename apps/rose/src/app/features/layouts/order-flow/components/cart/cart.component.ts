e1b45fc034a6cb59e9866990d9290e4460392811
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-08-04 19:22:11 +0300

    feat: 🎸 cart items page design

--- apps/rose/src/app/features/layouts/order-flow/components/cart/cart.component.ts
+++ apps/rose/src/app/features/layouts/order-flow/components/cart/cart.component.ts
@@ -1,5 +1,7 @@
 // @angular
 import { Component, inject } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 // @ngx
 import { TranslatePipe } from "@ngx-translate/core";
 // shared-services
@@ -8,10 +10,11 @@ import { TranslationService } from "@rose/core_services/translation/translation.
 import { ButtonComponent } from "@rose/shared_Components_ui/button/button.component";
 // Animation
 import { fadeTransition } from "@rose/core_services/translation/fade.animation";
+import { CustomInputComponent } from "@rose/shared_Components_ui/custom-input/custom-input.component";
 
 @Component({
   selector: "app-cart",
-  imports: [ButtonComponent, TranslatePipe],
+  imports: [RouterLink, ButtonComponent, TranslatePipe, ReactiveFormsModule, CustomInputComponent],
   templateUrl: "./cart.component.html",
   styleUrl: "./cart.component.scss",
   animations: [fadeTransition],
@@ -20,4 +23,8 @@ export class CartComponent {
   readonly translationService = inject(TranslationService);
 
   empty: boolean = false;
+
+  // quantityForm :FormGroup = new FormGroup({
+  //   quantity : new FormControl(0)
+  // })
 }
