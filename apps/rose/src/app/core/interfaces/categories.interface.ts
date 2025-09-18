598aefdf4e3bd30d7122d94532c8daf7de102759
Karim Ashraf <karim.ashraf@engaz.ai>
2025-09-18 17:09:35 +0300

    try fix

---
+++ apps/rose/src/app/core/interfaces/categories.interface.ts
@@ -0,0 +1,24 @@
+import { PaginationData } from "@angular-monorepo/products";
+
+export interface CategoryRes {
+  message: string;
+  metadata: PaginationData;
+  categories: Category[];
+}
+
+export interface CategoryOption {
+  label: string;
+  display: string;
+  id: string;
+}
+
+export interface Category {
+  _id: string;
+  name: string;
+  slug: string;
+  image: string;
+  createdAt: string;
+  updatedAt: string;
+  isSuperAdmin: boolean;
+  productsCount: number;
+}
