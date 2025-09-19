bff0cf635b4c39dcd177730a0ec395893d19bf19
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-09-19 14:02:27 +0300

    feat: 🎸 (soldOut)create sold out badge

---
+++ apps/rose/src/app/shared/components/ui/sold-out/soldOut.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-sold-out",
+  imports: [CommonModule],
+  templateUrl: "./soldOut.component.html",
+  styleUrl: "./soldOut.component.scss",
+})
+export class SoldOutComponent {}
