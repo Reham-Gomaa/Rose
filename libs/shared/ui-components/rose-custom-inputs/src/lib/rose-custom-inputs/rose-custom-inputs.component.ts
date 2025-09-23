89208c1d0d7868123e1e34663b6de7f363264a71
Karim Ashraf <karim.ashraf@engaz.ai>
2025-09-23 22:55:45 +0300

    ci: 🎡 remeove this component

--- libs/shared/ui-components/rose-custom-inputs/src/lib/rose-custom-inputs/rose-custom-inputs.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "lib-rose-custom-inputs",
-  imports: [],
-  templateUrl: "./rose-custom-inputs.component.html",
-  styleUrl: "./rose-custom-inputs.component.scss",
-})
-export class RoseCustomInputsComponent {}
