a8065b52bd90c2dc57ddf98b91ef3de502ba106a
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-09-24 12:43:30 +0300

    solve specs files conflict

--- apps/rose-dashboard/src/app/features/pages/overview/components/statistics/statistics.component.spec.ts
+++ apps/rose-dashboard/src/app/features/pages/overview/components/statistics/statistics.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { StatisticsComponent } from "./statistics.component";
+import { LoadingComponent } from "@rose_dashboard/features_layouts/loading/loading.component";
 
-<<<<<<<< HEAD:apps/rose-dashboard/src/app/features/pages/overview/components/statistics/statistics.component.spec.ts
 describe("StatisticsComponent", () => {
   let component: StatisticsComponent;
   let fixture: ComponentFixture<StatisticsComponent>;
@@ -12,25 +12,24 @@ describe("StatisticsComponent", () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(StatisticsComponent);
-========
-import { LoadingComponent } from "./loading.component";
 
-describe("LoadingComponent", () => {
-  let component: LoadingComponent;
-  let fixture: ComponentFixture<LoadingComponent>;
+    describe("LoadingComponent", () => {
+      let component: LoadingComponent;
+      let fixture: ComponentFixture<LoadingComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LoadingComponent],
-    }).compileComponents();
+      beforeEach(async () => {
+        await TestBed.configureTestingModule({
+          imports: [LoadingComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(LoadingComponent);
->>>>>>>> 69bb19998bdb50d0eb5dabf4caedf4d23d404deb:apps/rose-dashboard/src/app/features/layout/loading/loading.component.spec.ts
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(LoadingComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
+      it("should create", () => {
+        expect(component).toBeTruthy();
+      });
+    });
   });
 });
