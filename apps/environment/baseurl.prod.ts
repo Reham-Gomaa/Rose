b504881554104db39c2b259edc087f172c685020
karimprime <karimashraf000@gmail.com>
2025-09-26 17:12:05 +0300

    style: 💄 handle style and update env

--- apps/environment/baseurl.prod.ts
+++ apps/environment/baseurl.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   baseApiUrl: "https://flower.elevateegy.com/",
   runUrl: "https://rose-chi-nine.vercel.app/",
-  runUrlDashboard: "http://localhost:4200/#/dashboard/overview",
+  runUrlDashboard: "http://localhost:4200/#/w",
+  // runUrlDashboard: "https://rose-dashboard.vercel.app/#/dashboard/overview",
 };
