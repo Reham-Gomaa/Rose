c10dd9b2d0dbe26877a71850b3740afdf98a76ec
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-10-03 14:28:19 +0300

    feat: 🎸 (category)build ui-skeleton-responsive-optimize seo

--- apps/rose-dashboard/src/app/features/pages/overview/components/categories/categoriesReports.component.ts
+++ apps/rose-dashboard/src/app/features/pages/overview/components/categories/categoriesReports.component.ts
@@ -1,10 +1,36 @@
-import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import {
+  Component,
+  effect,
+  input,
+  InputSignal,
+  signal,
+  ViewEncapsulation,
+  WritableSignal,
+} from "@angular/core";
+import { TranslatePipe } from "@ngx-translate/core";
+// Shared_Interfaces
+import { Category } from "@rose_dashboard/core_interfaces/statistics";
+// Primeng
+import { RatingModule } from "primeng/rating";
+import { TableModule } from "primeng/table";
+import { TagModule } from "primeng/tag";
+import { Skeleton } from "primeng/skeleton";
 
 @Component({
   selector: "app-categories-reports",
-  imports: [CommonModule],
+  imports: [TableModule, TagModule, RatingModule, TranslatePipe, Skeleton],
   templateUrl: "./categoriesReports.component.html",
   styleUrl: "./categoriesReports.component.scss",
+  encapsulation: ViewEncapsulation.None,
 })
-export class CategoriesReportsComponent {}
+export class CategoriesReportsComponent {
+  categories: InputSignal<Category[]> = input([] as Category[]);
+  loading: WritableSignal<boolean> = signal(true);
+
+  constructor() {
+    effect(() => {
+      const data = this.categories();
+      this.loading.set(!data || data.length === 0);
+    });
+  }
+}
