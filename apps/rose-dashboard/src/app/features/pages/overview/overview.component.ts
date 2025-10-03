0d3b794518c7b87814dba60667ecffd849878f71
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-10-03 14:27:13 +0300

    feat: 🎸 (overview)build ui-skeleton-responsive-optimize seo

--- apps/rose-dashboard/src/app/features/pages/overview/overview.component.ts
+++ apps/rose-dashboard/src/app/features/pages/overview/overview.component.ts
@@ -1,38 +1,70 @@
-import { Component, DestroyRef, inject, OnInit } from "@angular/core";
-import { StatisticsComponent } from "./components/statistics/statistics.component";
-import { CategoriesReportsComponent } from "./components/categories/categoriesReports.component";
-import { TableComponent } from "./components/table/table.component";
+import { Component, DestroyRef, inject, OnInit, signal, WritableSignal } from "@angular/core";
+// RxJs
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { map } from "rxjs";
+// Shared_Services
+import { TranslationService } from "@angular-monorepo/services";
 import { StatisticsService } from "@rose_dashboard/shared_services/overview/statistics/statistics.service";
+// Shared_Comp
+import { CategoriesReportsComponent } from "./components/categories/categoriesReports.component";
+import { LowStockProductsComponent } from "./components/lowStock/lowStockProducts.component";
+import { OrderChartComponent } from "./components/orderChart/order-chart.component";
+import { RevenueChartComponent } from "./components/revenueChart/revenue-chart.component";
+import { StatisticsComponent } from "./components/statistics/statistics.component";
+import { TopSellingProductsComponent } from "./components/topSelling/topSellingProducts.component";
+
+import {
+  Category,
+  DailyRevenue,
+  LowStockProduct,
+  MonthlyRevenue,
+  OrdersByStatu,
+  Overall,
+  TopSellingProduct,
+} from "@rose_dashboard/core_interfaces/statistics";
 
 @Component({
   selector: "app-overview",
-  imports: [StatisticsComponent, CategoriesReportsComponent, TableComponent],
+  imports: [
+    StatisticsComponent,
+    CategoriesReportsComponent,
+    TopSellingProductsComponent,
+    LowStockProductsComponent,
+    OrderChartComponent,
+    RevenueChartComponent,
+  ],
   templateUrl: "./overview.component.html",
   styleUrl: "./overview.component.scss",
 })
 export class OverviewComponent implements OnInit {
+  private readonly translationService = inject(TranslationService);
   private readonly statisticsService = inject(StatisticsService);
   private readonly destroyRef = inject(DestroyRef);
 
+  overallReports: WritableSignal<Overall> = signal({} as Overall);
+  categories: WritableSignal<Category[]> = signal([] as Category[]);
+  orderByStatus: WritableSignal<OrdersByStatu[]> = signal([] as OrdersByStatu[]);
+  dailyRevenue: WritableSignal<DailyRevenue[]> = signal([] as DailyRevenue[]);
+  monthlyRevenue: WritableSignal<MonthlyRevenue[]> = signal([] as MonthlyRevenue[]);
+  topSellingPdts: WritableSignal<TopSellingProduct[]> = signal([] as TopSellingProduct[]);
+  lowStockPdts: WritableSignal<LowStockProduct[]> = signal([] as LowStockProduct[]);
+
   ngOnInit(): void {
-    this.getStatistics();
+    this.getAllStatistics();
   }
 
-  getStatistics() {
+  getAllStatistics() {
     this.statisticsService
       .getAllStatistics()
-      .pipe(
-        map((res) => {
-          return res.statistics.overall;
-        }),
-        takeUntilDestroyed(this.destroyRef)
-      )
+      .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: (res) => {
-          //this.statistics?.set(res);
-          //this.loading.set(false);
+          this.overallReports.set(res.statistics.overall);
+          this.categories.set(res.statistics.categories.filter((item) => item.totalProducts > 0));
+          this.orderByStatus.set(res.statistics.orders.ordersByStatus);
+          this.dailyRevenue.set(res.statistics.orders.dailyRevenue);
+          this.monthlyRevenue.set(res.statistics.orders.monthlyRevenue);
+          this.topSellingPdts.set(res.statistics.products.topSellingProducts);
+          this.lowStockPdts.set(res.statistics.products.lowStockProducts);
         },
       });
   }
