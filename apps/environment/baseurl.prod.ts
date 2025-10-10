677d7cedb850c1ff7ae0e15f24ab106db8400bff
karimprime <karim.ashraf@engaz.ai>
2025-10-10 17:10:26 +0300

    ci: 🎡 release Dashboard-Rose

--- apps/environment/baseurl.prod.ts
+++ apps/environment/baseurl.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   baseApiUrl: "https://flower.elevateegy.com/",
   runUrl: "https://rose-chi-nine.vercel.app/",
-  runUrlDashboard: "http://localhost:4200/#/",
-  // runUrlDashboard: "https://rose-dashboard.vercel.app/#/dashboard/overview",
+  // runUrlDashboard: "http://localhost:4200/#/",
+  runUrlDashboard: "https://rose-dashboard.vercel.app/#/dashboard/overview",
 };
