82b8789b09372482bf301fd611f07c2cabb7bea7
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-10-16 13:02:37 +0300

    refactor: 💡 (headerInterceptor)update user token

--- libs/core/src/lib/interceptors/header.interceptor.ts
+++ libs/core/src/lib/interceptors/header.interceptor.ts
@@ -8,7 +8,7 @@ export const headingInterceptor: HttpInterceptorFn = (req, next) => {
 
   const token = _storage.getItem("authToken");
   const userToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjhhMDQ4MmVhOGJjYTMwN2Y5ZGRhNGJlIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NjAwOTcyNDV9.CySfecJbsVqmBi8jUNolPe2y2vAYRsThNaSsW4MFE4g";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjg4MzhkMDdhOGJjYTMwN2Y5ZDZhMzBkIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NjA2MDE3MTV9.EzYOU7rC6SgFOIB8ILS53ICzMnQMzHTd7WPAyK3uD20";
 
   if (token) {
     if (!req.url.includes("statistics")) {
